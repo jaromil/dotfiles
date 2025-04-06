fc01a4822157aa0d721e9cb2d74ec07cdcefc614
Jaromil <jaromil@dyne.org>
2025-04-06 14:14:42 +0200

    windows fixes and small refactor

--- loader.sh
+++ loader.sh
@@ -10,7 +10,7 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Source the dotfiles (order matters)
 
-for DOTFILE in "$DOTFILES_DIR"/system/{function,function_*,path,env,alias,windows,prompt,extensions,startmenu,onedrive,jumptable}; do
+for DOTFILE in "$DOTFILES_DIR"/system/{function,function_*,path,env,alias,windows,prompt,extensions,jumptable}; do
   [ -f "$DOTFILE" ] && . "$DOTFILE"
 done
 
